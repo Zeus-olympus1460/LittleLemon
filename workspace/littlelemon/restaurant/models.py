40641b2bb398f98fefae682b2522a9960a64cb08
Eddie <djangoapps4@gmail.com>
2025-09-13 01:19:48 +0100

    added_booking_and_menu_model

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=1)
+    booking_date = models.DateField()
+    
+    def __str__(self):
+        return f" Booking for {self.name} on {self.booking_date} for {self.no_of_guests} guests"
+
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title + " - $" + str(self.price) + " - " + str(self.inventory) + " left in stock"
